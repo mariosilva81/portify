fbee7876db5ee8cd5ab139b0ca4d8f9700292fe0
mariosilva81 <mariosilva.81@icloud.com>
2023-07-06 17:23:09 -0300

    Refactor: Ajuste nas rotas e na Dashboard Page

--- src/routes/RoutesMain.tsx
+++ src/routes/RoutesMain.tsx
@@ -2,28 +2,26 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "../pages/HomePage";
 import { RegisterPage } from "../pages/RegisterPage";
 import { LoginPage } from "../pages/LoginPage";
-import { UnpublishedHomePage } from "../pages/DashboardPage/UnpublishedHomePage";
-import { PublishedHomePage } from "../pages/DashboardPage/PublishedHomePage";
 import { ProfilePage } from "../pages/DashboardPage/ProfilePage";
 import { ProjectsPage } from "../pages/DashboardPage/ProjectsPage";
 import { PortfolioPage } from "../pages/PortfolioPage";
 import { PublicsRoutes } from "./PublicsRoutes";
 import { PrivatesRoutes } from "./PrivatesRoutes";
+import { DashboardPage } from '../pages/DashboardPage';
 
 export const RoutesMain = () => {
 	return (
 		<Routes>
 			<Route element={<PublicsRoutes />}>
-				<Route path="/" element={<HomePage />} />
-				<Route path="/login" element={<LoginPage />} />
-				<Route path="/register" element={<RegisterPage />} />
-				<Route path="/dashboard/unpublished" element={<UnpublishedHomePage />} />
-				<Route path="/dashboard/published" element={<PublishedHomePage />} />
-				<Route path="/dashboard/profile" element={<ProfilePage />} />
-				<Route path="/dashboard/projects" element={<ProjectsPage />} />
 			</Route>
 			<Route element={<PrivatesRoutes />}>
 			</Route>
+				<Route path="/" element={<HomePage />} />
+				<Route path="/login" element={<LoginPage />} />
+				<Route path="/register" element={<RegisterPage />} />
+				<Route path="/dashboard" element={<DashboardPage />} />
+				<Route path="/dashboard/profile" element={<ProfilePage />} />
+				<Route path="/dashboard/projects" element={<ProjectsPage />} />
 				<Route path="/portfolio" element={<PortfolioPage />} />
 		</Routes>
 	);
