fcdd05f60da1591ae16bcac35241d8d38c495d4f
mariosilva81 <mariosilva.81@icloud.com>
2023-07-07 15:05:08 -0300

    Fix: Correção no modal

--- src/pages/DashboardPage/ProjectsPage/components/AddProjectModal/index.tsx
+++ src/pages/DashboardPage/ProjectsPage/components/AddProjectModal/index.tsx
@@ -1,18 +1,32 @@
-// import { useKeyDown } from "../../../../../../hooks/useKeyDown"
-// import { useOutsideClick } from "../../../../../../hooks/useOutsideClick"
+import { useState, MutableRefObject, RefObject } from 'react';
+import { IUseKeyDown, useKeyDown } from "../../../../../hooks/useKeyDown";
+import { useOutsideClick } from "../../../../../hooks/useOutsideClick";
+import CloseButtonBlack from "../../../../../assets/icons/close-black.png"
+import CloseButtonWhite from "../../../../../assets/icons/close-white.png"
 
 export const AddProjectModal = () => {
-  // const modalRef = useOutsideClick(() => {
-  //   // setIsOpenAdd(false);
-  // })
+  const [isOpen, setIsOpen] = useState(false);
 
-  // const buttonRef = useKeyDown("Escape", (element) => element.click());
+  // const modalRef: RefObject<HTMLDivElement> = useOutsideClick({
+  //   callback: () => {
+  //     setIsOpen(false);
+  //   },
+  // });
+
+  // const buttonRef: MutableRefObject<IUseKeyDown> = useKeyDown("Escape", (element: { click: () => void; }) => {
+  //   element.click()
+  // });
 
   return (
     <div role="dialog">
-      {/* <div ref={modalRef}>
-            
-      </div> */}
+      <div ref={modalRef}>
+        <h1>Modal de Criar Projeto</h1>
+        <img
+          src={CloseButtonBlack} 
+          ref={buttonRef} 
+          onClick={() => setIsOpen(false)}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
