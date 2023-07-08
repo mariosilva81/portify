92b072d5f4cb5cb14d3154598657e8f6994faa39
Ronaldo França <ronaldofrancajr@gmail.com>
2023-07-08 10:11:47 -0300

    Styles: Alterando estilo global do footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,15 @@
 // import LogoWhite from "../../assets/images/logo-white.png";
 import LogoBlack from "../../assets/images/logo-black.png";
-import { FooterStyled } from "./styles";
+
+import { StyledFooter } from "./styles";
 
 export const Footer = () => {
   return (
-    <FooterStyled>
+    <StyledFooter>
       {/* {blackMode ? (<img src={LogoWhite} alt="portify" />
       ) : <img src={LogoBlack} alt="portify" />} */}
       <img src={LogoBlack} alt="portify" />
       <p>Todos os direitos reservados - Kenzie Academy Brasil</p>
-    </FooterStyled>
+    </StyledFooter>
   );
 };
