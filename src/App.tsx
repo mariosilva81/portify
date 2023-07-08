8147986472153af074c094a01ed3005347a619cc
wilrrama <wilrrama@hotmail.com>
2023-07-08 16:10:44 -0300

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,33 +4,43 @@ import { RoutesMain } from "./routes/RoutesMain";
 import { GlobalStyles } from "./styles/globalStyles";
 import { ResetStyles } from "./styles/resetStyles";
 import { UserProvider } from "./providers/UserContext/UserContext";
-import { PortfolioProvider } from './providers/PortfolioContext/PortfolioContext';
-import { ProjectsProvider } from './providers/ProjectsContext/ProjectsContext';
+import { PortfolioProvider } from "./providers/PortfolioContext/PortfolioContext";
+import { ProjectsProvider } from "./providers/ProjectsContext/ProjectsContext";
+import { ThemeProvider } from "styled-components";
+import light from "./styles/themes/light";
+import dark from "./styles/themes/dark";
+import { useState } from "react";
 
 export const App = () => {
+  const [currentTheme] = useState(localStorage.getItem("@COLORTHEME"));
+
+  const theme = currentTheme === "Escuro" ? dark : light;
+
   return (
     <>
-      <ResetStyles />
-      <GlobalStyles />
-			<UserProvider>
-					<ProjectsProvider>
-				<PortfolioProvider>
-      			<RoutesMain />
-				</PortfolioProvider>
-					</ProjectsProvider>
-			</UserProvider>
-      <ToastContainer 
-				position="top-right"
-				autoClose={3000}
-				hideProgressBar={false}
-				newestOnTop={false}
-				closeOnClick
-				rtl={false}
-				pauseOnFocusLoss
-				draggable
-				pauseOnHover
-				theme="colored"
-			/>
+      <ThemeProvider theme={theme}>
+        <ResetStyles />
+        <GlobalStyles />
+        <UserProvider>
+          <ProjectsProvider>
+            <PortfolioProvider>
+              <RoutesMain />
+            </PortfolioProvider>
+          </ProjectsProvider>
+        </UserProvider>
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </ThemeProvider>
     </>
-  )
-}
+  );
+};
